6477d2deb2ab5547f7024866bb9c9591309993e3
Alina Saetova <alinasaetowa@gmail.com>
2020-04-02 20:02:11 +0300

    fix tabs

--- app/src/main/java/ru/itis/cats_facts/MainActivity.kt
+++ app/src/main/java/ru/itis/cats_facts/MainActivity.kt
@@ -1,30 +1,46 @@
 package ru.itis.cats_facts
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import ru.itis.cats_facts.ui.main.SectionsPagerAdapter
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.itis.cats_facts.view.FavouritesFragment
+import ru.itis.cats_facts.view.SearchFragment
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val titles = arrayOf("Search", "Favourites")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        setSupportActionBar(toolbar)
+        view_pager.adapter = ViewPagerAdapter(this)
+        TabLayoutMediator(tabs, view_pager,
+            TabConfigurationStrategy { tab: TabLayout.Tab, position: Int ->
+                tab.text = titles[position]
+            }
+        ).attach()
     }
+}
+
+
+class ViewPagerAdapter(mainActivity: MainActivity) : FragmentStateAdapter(mainActivity) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> return SearchFragment()
+            1 -> return FavouritesFragment()
+        }
+        return FavouritesFragment()
+    }
+
 }
